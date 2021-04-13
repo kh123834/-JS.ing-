a1cd586b0db96ecbd67c15af2748f5f6017bda86
HYEON-KIM <82227075+kh123834@users.noreply.github.com>
2021-04-13 23:38:39 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+const title = document.querySelector("#title");
+
+function handleClick() {
+    title.style.color = "blue";
+}
+
+title.addEventListener("click", handleClick);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const calculater = {
+    plus: function (a, b)
+    {
+    return a+b;
+    },
+    minus: function (a, b)
+    {
+    return a-b;
+    },
+    multiple: function (a, b)
+    {
+    return a*b;
+    },
+    divide: function (a, b)
+    {
+    return a/b;
+    },
+    remainder: function (a, b)
+    {
+    return a%b;
+    },
+    power: function (a, b)
+    {
+    return a**b;
+    }
+    }
+    
+    const plus = calculater.plus (5, 5)
+    console.log(plus)
+    
+    const minus = calculater.minus (10, 10)
+    console.log(minus)
+    
+    const multiple = calculater.multiple (10, 10)
+    console.log(multiple)
+    
+    const divide = calculater.divide (2323, 10)
+    console.log(divide)
+    
+    const remainder = calculater.remainder (5, 10)
+    console.log(remainder)
+    
+    const power = calculater.power (3, 20)
+    console.log(power)*/
